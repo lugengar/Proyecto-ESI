aa8231e687ff6ca18d08fcd8dfc8228551ce02d7
lugengar <luciano.barbinii@gmail.com>
2024-08-26 18:34:56 -0300

    a

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en-es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="https://abc.gob.ar/core/themes/abc/favicon.ico" type="image/vnd.microsoft.icon">
+    <title>Ofertas de Educación Superior Región 6</title>
+    <meta name="description" content="Ofertas de Educación Superior Región 6">
+</head>
+<body>    
+    <div class="container">
+        <main class="carrusel">
+            <div class="imagenes">
+                <div class="imagen activo" style="background-image: url('imagenes/otros/estudiantes.jpg');"></div>
+                <div class="imagen" style="background-image: url('imagenes/otros/gente.jpg');"></div>
+                <div class="imagen" style="background-image: url('imagenes/otros/graduados.jpg');"></div>
+               
+            </div>
+            <div class="filtro">
+                <div class="contenidotexto">
+                    <h1 class="texto1">Ofertas de Educación Superior Región 6</h1>
+                    
+                </div>
+                <div class="contenidotexto">
+                    <h1 class="texto2">Ahora es más fácil encontrar la universidad adecuada.</h1>
+                </div>
+                <div class="circulos">
+                    <span class="circulo activo"></span>
+                    <span class="circulo"></span>
+                    <span class="circulo"></span>
+                   
+                </div>
+            </div>
+        </main>
+        <header class="header" id="header">
+            <a href="index.php" class="logo_pba_horizontal " ></a>
+        </header>
+        <main class="main">
+
+        </main>
+
+        <footer class="footer">
+
+        </footer>
+    </div>
+</body>
+</html>
+<script src="carrusel.js"></script>
+
