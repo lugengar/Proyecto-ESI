eff102b26b65ab506784c16c9bdb4ae91ae6e6e5
lugengar <luciano.barbinii@gmail.com>
2024-08-28 18:07:48 -0300

    a

--- carrusel.js
+++ carrusel.js
@@ -2,21 +2,6 @@
 const imagenes = document.querySelectorAll('.imagen');
 
 if(imagenes.length > 1){
-circulos2.forEach((circulo) => {
-    circulo.addEventListener('click', () => {
-        let imagenes = circulo.parentNode.parentNode.querySelectorAll(".imagenuni")
-        let circulos = circulo.parentNode.querySelectorAll(".circulo2")
-        let index = Array.from(circulos).indexOf(circulo);
-        circulos.forEach(circulo => circulo.classList.remove('activo'));
-        imagenes.forEach(imagen => imagen.classList.remove('activo'));
-        
-        circulo.classList.add('activo');
-        imagenes[index].classList.add('activo');
-    });
-});
-const circulos2 = document.querySelectorAll('.circulo2');
-    
-
 const circulos = document.querySelectorAll('.circulo');
 let currentIndex = 0;
 let intervalId;
